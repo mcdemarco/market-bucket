4cae067c8b291cc67dfdb1f0a82adf9c625b1960
M. C. DeMarco <mcd@mcdemarco.net>
2014-10-16 22:57:36 -0400

    Fix for issues saving to archive.

--- js/main.js
+++ js/main.js
@@ -604,11 +604,6 @@ context.item = (function () {
 			alert("No item text entered.");
 			return;
 		}
-		if (channelArray[channelId].hasOwnProperty("listTypes") && !$("input[name=bucketBucket]").is(":checked")) {
-			//This shouldn't be reachable anymore...
-			alert("No list selected for item.");
-			return;
-		}
 		//The edit case.
 		if ($("#editItemId").val() != "") {
 			if (message == messageTextArray) {
