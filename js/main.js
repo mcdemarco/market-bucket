e3317f5a6901f29811df3aef3afd57a0f38ec657
M. C. DeMarco <mcd@mcdemarco.net>
2023-05-24 14:00:19 -0400

    Up version for release

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var marketBucket = {};
 	var channelArray = {};
 	var messageTextArray = {};
 	var updateArgs = {include_raw: 1};
-	var version = "2.3.1-beta";
+	var version = "2.3.1";
 
 
 context.init = (function () {
